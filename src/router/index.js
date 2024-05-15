f01847296b066c99f39c6dd5274a4b69c877e568
tianyuerong <tianyuerong@vchangyi.com>
2024-05-15 14:23:26 +0800

    补充

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Catalog from '../views/Catalog.vue'
-const router = createRouter({
+import HelloWorld from '../components/HelloWorld.vue'
+import htmlToImg from '../views/html-to-img.vue'
+export const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
@@ -8,8 +10,12 @@ const router = createRouter({
       component: Catalog
     },
     {
-      path:"hello",
-      component
+      path:"/hello",
+      component: HelloWorld
+    },
+    {
+      path:"/htmlToImg",
+      component: htmlToImg
     }
   ],
 })
--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -1,22 +1,37 @@
 <template>
   <div class="catalog">
-    <router-link to="/">Go to Home</router-link>
-    <router-link to="/about">Go to About</router-link>
+    <h1><p>目录</p></h1>
+    <ul>
+      <li><router-link to="/htmlToImg">html转图片 （使用html2canvas）</router-link></li>
+      <li><router-link to="/about">js预览pdf</router-link></li>
+      <li><router-link to="/about">预览图片</router-link></li>
+      <li><router-link to="/about">预览视频</router-link></li>
+      <li><router-link to="/about">播放音频</router-link></li>
+      <li><router-link to="/about">录制音频</router-link></li>
+      <li><router-link to="/about">预览office</router-link></li>
+    </ul>
   </div>
 </template>
-
-<script>
-export default {
-  name:'Catalog',
-  data(){
-    return{}
-  },
-  mounted(){
-
-  }
+<script setup>
+import { reactive,ref } from 'vue'
+const obj = reactive({
+  foo: ref(0)
+})
+console.log(obj.foo)
+let {foo} = obj //bad
+let foo1 = obj.foo //bad
+function increment() {
+  obj.foo++;
+  foo++;
+  foo1++
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+h1{
+  p{
+    color: #333;
+    font-size: 28px;
+  }
+}
 </style>
